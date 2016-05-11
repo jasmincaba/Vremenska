cf29a2053bb34b99a0f111f808ae4d24d52c1c69
jasmincaba <jasmincaba@gmail.com>
2016-05-11 15:03:50 +0200

    Nakon ubacivanja ArrayAdaptera

--- app/src/main/java/com/example/jasko/vremenska/MainActivity.java
+++ app/src/main/java/com/example/jasko/vremenska/MainActivity.java
@@ -1,18 +1,20 @@
 package com.example.jasko.vremenska;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,13 +54,14 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends android.support.v4.app.Fragment {
 
+        ArrayAdapter<String> mForecastAdapter;
         public PlaceholderFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
 
             String[] forecastArray={
                     "Mon 6/23 - Sunny - 31/17",
@@ -70,6 +73,13 @@ public class MainActivity extends ActionBarActivity {
                     "Sun 6/29 - Sunny - 20/7"
             };
             List<String>weekForecast=new ArrayList<String>(Arrays.asList(forecastArray));
+            mForecastAdapter=
+                    new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,
+                            R.id.list_item_forecast_textview,weekForecast);
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
             return rootView;
         }
     }
